60f3cc80fc2c15126706eb875b18b14d3825761e
roadkillsanta <ericnie7@gmail.com>
2016-10-05 15:04:28 -0400

    Update CMD.java

--- CMD.java
+++ CMD.java
@@ -1,13 +1,25 @@
 package main;
 
 public class CMD extends ADAPTER{
-	
+	public String sys=System.getProperty("os.name")
+	public static Boolean runcommand(command){
+		Process process=Runtime.getRuntime().exec(command);
+		return process.getExitValue();
+	}
 	public static Boolean interpreter(String cmd){
 		Boolean rtrn=true;
 		String sys=System.getProperty("os.name");
 		if(sys.contains("Win")){}
+		else if
+		else{
+			public
+		}
 		if(cmd==""){}
-		else if(cmd.contains("run")){ADAPTER.runCommand("death");}
+		else if(cmd.contains("run")){
+			finishedcommand=cmd.replace("run ", "");
+			returnstuff=ADAPTER.runCommand(finishedcommand);
+			return returnstuff;
+		}
 		else if(cmd==""){}
 		else if(cmd==""){}
 		else if(cmd==""){}
